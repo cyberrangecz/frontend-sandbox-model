b9e52890fcb25bee3c2e117610ff8d01d68c97c3
Martin Hamerník <445720@mail.muni.cz>
2020-03-27 13:14:09 +0100

    Initial commit

--- projects/kypo-sandbox-model/src/public-api.ts
+++ projects/kypo-sandbox-model/src/public-api.ts
@@ -2,6 +2,32 @@
  * Public API Surface of kypo-sandbox-model
  */
 
-export * from './lib/kypo-sandbox-model.service';
-export * from './lib/kypo-sandbox-model.component';
-export * from './lib/kypo-sandbox-model.module';
+// DEFINITION
+export {SandboxDefinition} from './lib/definition/sandbox-definition';
+
+// POOL
+export {Pool} from './lib/pool/pool';
+
+// POOL REQUEST
+export {Request} from './lib/pool-request/request';
+export {AllocationRequest} from './lib/pool-request/allocation-request';
+export {CleanupRequest} from './lib/pool-request/cleanup-request';
+
+// POOL REQUEST STAGES
+export {AllocationRequestStage} from './lib/pool-request/stage/allocation-request-stage';
+export {AnsibleAllocationStage} from './lib/pool-request/stage/ansible-allocation-stage';
+export {AnsibleCleanupStage} from './lib/pool-request/stage/ansible-cleanup-stage';
+export {CleanupRequestStage} from './lib/pool-request/stage/cleanup-request-stage';
+export {OpenStackAllocationStage} from './lib/pool-request/stage/open-stack-allocation-stage';
+export {OpenStackCleanupStage} from './lib/pool-request/stage/open-stack-cleanup-stage';
+export {RequestStage} from './lib/pool-request/stage/request-stage';
+
+// SANDBOX INSTANCE
+export {SandboxInstance} from './lib/sandbox-instance/sandbox-instance';
+
+// SANDBOX INSTANCE RESOURCE
+export {SandboxResource} from './lib/sandbox-instance/sandbox-instance-resource/sandbox-resource';
+
+// ENUMS
+export {RequestStageState} from './lib/enums/request-stage-state.enum';
+export {RequestStageType} from './lib/enums/request-stage-type.enum';
