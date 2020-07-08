1516547a3e4f8f7aa6362901dcdeb038b701f371
Martin Hamernik <445720@mail.muni.cz>
2020-07-08 11:30:37 +0200

    Remove dependency on kypo-common

--- projects/kypo-sandbox-model/src/lib/pool-request/request.ts
+++ projects/kypo-sandbox-model/src/lib/pool-request/request.ts
@@ -1,22 +1,13 @@
-import { KypoDateTimeFormatPipe } from 'kypo-common';
-
 /**
  * Class representing pool request
  */
 export abstract class Request {
   id: number;
   allocationUnitId: number;
-  createdAtFormatted: string;
+  createdAt: Date;
   readonly type: string;
-  private _createdAt: Date;
 
   protected constructor(type: string) {
     this.type = type;
-  }
-
-  set createdAt(value: Date) {
-    this._createdAt = value;
-    const dateTimeFormatter = new KypoDateTimeFormatPipe('en-EN');
-    this.createdAtFormatted = dateTimeFormatter.transform(value);
   }
 }
