6dc6a30420c4c9327aca43043a04612a12c00389
Martin Hamernik <445720@mail.muni.cz>
2020-07-08 11:19:34 +0200

    Remove allocation stage id attr

--- projects/kypo-sandbox-model/src/lib/pool-request/stage/networking-ansible-cleanup-stage.ts
+++ projects/kypo-sandbox-model/src/lib/pool-request/stage/networking-ansible-cleanup-stage.ts
@@ -2,8 +2,6 @@ import { RequestStageType } from '../../enums/request-stage-type.enum';
 import { CleanupRequestStage } from './cleanup-request-stage';
 
 export class NetworkingAnsibleCleanupStage extends CleanupRequestStage {
-  allocationStageId: number;
-
   constructor() {
     super(RequestStageType.NETWORKING_ANSIBLE_CLEANUP);
   }
