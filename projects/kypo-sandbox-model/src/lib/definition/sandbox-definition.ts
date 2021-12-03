48771fda3847c40c62448aa5546dae5f17d3cc2f
Erik Kočamba <445661@mail.muni.cz>
2021-12-03 15:18:09 +0100

    Resolve "Add created by field to SD"

--- projects/kypo-sandbox-model/src/lib/definition/sandbox-definition.ts
+++ projects/kypo-sandbox-model/src/lib/definition/sandbox-definition.ts
@@ -2,11 +2,14 @@
  * Class representing sandbox definition in a system
  */
 
+import { CreatedBy } from '../pool/created-by';
+
 export class SandboxDefinition {
   id: number;
   title: string;
   url: string;
   rev: string;
+  createdBy: CreatedBy;
 
   toString(): string {
     return this.title;
